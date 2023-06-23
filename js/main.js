215286c38daed27c1d1548cc5f292ec2962fcd07
Ethan <templee3@wwu.edu>
2023-06-22 20:13:24 -0700

    mining works now

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 //game currencies
-var dirt = new Decimal(0);
+var game = {
+    dirt: new Decimal(0)
+};
 
 function mineDirt()
 {
-    dirt.add(1);
-    document.getElementById("dirt").innerHTML = dirt;
+    game.dirt = game.dirt.add(1);
+    document.getElementById("dirtAmount").innerHTML = game.dirt;
 }
