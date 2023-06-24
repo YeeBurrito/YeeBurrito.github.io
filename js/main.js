fc8f50f555bfbb361c6677dccf6f5362b546680c
Ethan <templee3@wwu.edu>
2023-06-24 00:55:51 -0700

    updated style, added game saving and loading

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
 //game currencies
 var game = {
+    lastSave: 0,
     dirt: new Decimal(0)
 };
+
+setInterval(updateSeconds, 1000)
+
+//update the game every second
+function updateSeconds()
+{
+    save();
+}
+
+function save()
+{
+    game.lastSave = Date.now();
+    localStorage.setItem("grimeSave", JSON.stringify(game));
+    localStorage.setItem("grimeLastSaved", game.lastSave);
+}
+
+function load()
+{
+    var savegame = JSON.parse(localStorage.getItem("grimeSave"));;
+    var lastSave = localStorage.getItem("grimeLastSaved");
+    if (savegame !== null)
+    {
+        //set each variable in the savegame to the game
+        for (var i in savegame)
+        {
+            game[i] = new Decimal(savegame[i]);
+        }
+
+        document.getElementById("dirtAmount").innerHTML = game.dirt;
+    }
+}
 
 function mineDirt()
 {
