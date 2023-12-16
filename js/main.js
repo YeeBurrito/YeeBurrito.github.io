1cd71ada1b5a88f3688c0dbc2432ad3bd9d160cc
Ethan <81653895+YeeBurrito@users.noreply.github.com>
2023-12-15 21:58:13 -0800

    got the columns to work

--- js/main.js
+++ js/main.js
@@ -39,13 +39,13 @@ function load()
             document.getElementById("popupText").innerHTML = "<p>In the mystical realm of IDK bruh, where magic and mystery intertwine, an ancient prophecy echoes through the ages. You, adventurer, have been chosen by destiny to unveil the secrets hidden deep within the IDK bruh Abyss.</p><p>As you stand at the entrance of the enchanted mine, the ethereal voices of the Elders resonate in your mind, guiding you on a quest of unparalleled significance. The IDK bruh Abyss, sealed by the Shattering eons ago, holds the key to unimaginable riches and arcane power.</p><p>Brave the treacherous depths, mine rare materials, and uncover the lost history of IDK bruh. The choices you make will shape the fate of this realm and determine whether magic will once again flourish or fade into obscurity.</p><p>Forge your legacy, adventurer, and let the Chronicles of IDK bruh unfold as you embark on the epic journey that awaits within the IDK bruh Abyss.</p>";
             document.getElementById("proceedBtn").addEventListener("click", function() {
                 document.getElementById("popup").style.display = "none";
-                document.getElementById('mainContent').style.display = 'block';
+                document.getElementById('mainContent').style.display = 'flex';
                 game.lore_index++;
             });
         }
         else
         {
-            document.getElementById('mainContent').style.display = 'block';
+            document.getElementById('mainContent').style.display = 'flex';
         }
 
         document.getElementById("dirtAmount").innerHTML = game.dirt;
