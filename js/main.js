631ff57c794f52b34fcfec0d5c846a717bfe5654
Ethan <Ethan@MacBook-Pro-114.local>
2024-01-23 17:41:34 -0800

    fixed a lot of tab bugs, reworked saving and loading

--- js/main.js
+++ js/main.js
@@ -1,29 +1,61 @@
 //game currencies
 var game = {
     lastSave: 0,
-    progressIndex: 0,
-    infoTextProgressIndex: 1,
+    progressIndex: -1,
+    infoTextProgressIndex: 0,
     infoTextLocation: 0,
     resources: {
+        gold: new Decimal(0),
         dirt: new Decimal(0),
+    },
+    buyables: {
+        dirtMiners: {
+            cost: new Decimal(10),
+            amount: new Decimal(0),
+            costIncrease: new Decimal(1.4),
+            dirtPerSecond: new Decimal(0.1)
+        },
+        dirtUpgrades: {
+            cost: new Decimal(10),
+            powerAmount: new Decimal(0),
+            speedAmount: new Decimal(0),
+            costIncrease: new Decimal(1.3),
+            dirtMiningSpeed: new Decimal(5), //amount of time in seconds to mine dirt
+            dirtMiningSpeedScaling: new Decimal(0.9),
+            dirtMiningPower: new Decimal(1),
+            dirtMiningPowerScaling: new Decimal(1.1)
+        }
     }
 };
 
 //game lore texts
 const infoText = [
     "In the mystical realm of IDK bruh, where magic and mystery intertwine, an ancient prophecy echoes through the ages. You, adventurer, have been chosen by destiny to unveil the secrets hidden deep within the IDK bruh Abyss. As you stand at the entrance of the enchanted mine, the ethereal voices of the Elders resonate in your mind, guiding you on a quest of unparalleled significance. The IDK bruh Abyss, sealed by the Shattering eons ago, holds the key to unimaginable riches and arcane power. Brave the treacherous depths, mine rare materials, and uncover the lost history of IDK bruh. The choices you make will shape the fate of this realm and determine whether magic will once again flourish or fade into obscurity. Forge your legacy, adventurer, and let the Chronicles of IDK bruh unfold as you embark on the epic journey that awaits within the IDK bruh Abyss.",
-    "This is the third lore text."
+    "As you delve deeper into the mine, you begin to notice a peculiar glow emanating from the tenth clump of dirt you unearth. Examining it closely, you discover that this dirt harbors slightly magical properties, shimmering with an ethereal essence. Curiosity guiding your steps, you ascend from the mine's depths and emerge into the daylight. Before you stretches a quaint town, its cobblestone streets lined with shops and stalls. The townsfolk, unaware of the enchantment hidden beneath the surface, greet you with friendly smiles and curious glances.Your newfound magical dirt becomes a topic of interest among the townspeople. Eager to learn more, you engage in conversations with the locals. It doesn't take long before you stumble upon a seasoned merchant who recognizes the unique properties of your unearthed resources. The merchant introduces you to the town's marketplace, where you can trade your magical dirt and other treasures for precious gold. The town offers various services and upgrades, allowing you to enhance your mining abilities and make your subterranean endeavors even more fruitful."
 ];
 const popupText = [
     "In the mystical realm of IDK bruh, where magic and mystery intertwine, an ancient prophecy echoes through the ages. You, adventurer, have been chosen by destiny to unveil the secrets hidden deep within the IDK bruh Abyss.</p><p>As you stand at the entrance of the enchanted mine, the ethereal voices of the Elders resonate in your mind, guiding you on a quest of unparalleled significance. The IDK bruh Abyss, sealed by the Shattering eons ago, holds the key to unimaginable riches and arcane power.</p><p>Brave the treacherous depths, mine rare materials, and uncover the lost history of IDK bruh. The choices you make will shape the fate of this realm and determine whether magic will once again flourish or fade into obscurity.</p><p>Forge your legacy, adventurer, and let the Chronicles of IDK bruh unfold as you embark on the epic journey that awaits within the IDK bruh Abyss.",
+    "You have unlocked the town tab.",
 ];
 
 setInterval(updateSeconds, 1000)
+setInterval(update, 50)
 
 //update the game every second
 function updateSeconds()
 {
     save();
+    checkProgress();
+}
+
+//update the game every 50 milliseconds
+function update()
+{
+    //update the display of each resource
+    for (var key in game.resources)
+    {
+        document.getElementById(key + "Amount").innerHTML = game.resources[key];
+    }
 }
 
 function save()
@@ -33,11 +65,24 @@ function save()
     localStorage.setItem("localLastSaved", game.lastSave);
 }
 
+function resetSave()
+{
+    localStorage.removeItem("localSave");
+    localStorage.removeItem("localLastSaved");
+    location.reload();
+}
+
 function load()
 {
     var savegame = JSON.parse(localStorage.getItem("localSave"));;
     var lastSave = localStorage.getItem("localLastSaved");
-    if (savegame !== null)
+    if (savegame == null)
+    {
+        game = game;
+        save();
+        location.reload();
+    }
+    else
     {
         //set each variable in the savegame to the game
         game.progressIndex = savegame.progressIndex;
@@ -45,7 +90,7 @@ function load()
         game.infoTextLocation = savegame.infoTextLocation;
         if (savegame.infoTextLocation == null || savegame.infoTextLocation == undefined)
         {
-            game.infoTextLocation = 1;
+            game.infoTextLocation = 0;
         }
 
         //convert each resource to a Decimal
@@ -54,15 +99,17 @@ function load()
             game.resources[key] = new Decimal(savegame.resources[key]);
         }
         
-        if (game.progressIndex == 0)
+        if (game.progressIndex == -1)
         {
             document.getElementById("popup").style.display = "block";
-            document.getElementById("popupText").innerHTML = popupText[game.progressIndex];
+            document.getElementById("popupText").innerHTML = popupText[0];
             document.getElementById("proceedBtn").addEventListener("click", function() {
                 document.getElementById("popup").style.display = "none";
                 document.getElementById('mainContent').style.display = 'flex';
-                game.progressIndex++;
-                game.infoTextProgressIndex = 1;
+                if (game.progressIndex == -1)
+                {
+                    game.progressIndex = 0;
+                }
             });
         }
         else
@@ -70,11 +117,11 @@ function load()
             document.getElementById('mainContent').style.display = 'flex';
         }
 
-        if (game.progressIndex < 2)
+        if (game.progressIndex < 1)
         {
            document.getElementById("townTab").style.display = "none";
         }
-        if (game.progressIndex < 3)
+        if (game.progressIndex < 2)
         {
             document.getElementById("upgradesTab").style.display = "none";
         }
@@ -82,6 +129,43 @@ function load()
         displayInfoText(game.infoTextLocation)
         document.getElementById("dirtAmount").innerHTML = game.resources.dirt;
         showTab('mine');
+    }
+}
+
+function checkProgress()
+{
+    currentProgressIndex = game.progressIndex;
+    let progressConditions = [
+        function() { return game.resources.dirt.cmp(10) >= 0;},
+    ];
+
+    if (currentProgressIndex < progressConditions.length && typeof progressConditions[currentProgressIndex] === 'function') {
+        if (progressConditions[currentProgressIndex]()) {
+            game.progressIndex++;
+            game.infoTextProgressIndex++;
+            document.getElementById("popup").style.display = "block";
+            document.getElementById("popupText").innerHTML = popupText[game.progressIndex];
+            displayInfoText(game.infoTextLocation);
+            updateProgress(game.progressIndex);
+        }
+    }
+}
+
+function updateProgress(progressIndex)
+{
+    let progressUpdates = [
+        function() { //Progress 0, doesnt do anything
+        },
+        function() { //Progress 1, unlocks the town tab
+            document.getElementById("townTab").style.display = "block";
+        }
+    ];
+    for (var i = 0; i < progressUpdates.length; i++)
+    {
+        if (i == progressIndex)
+        {
+            progressUpdates[i]();
+        }
     }
 }
 
@@ -141,8 +225,14 @@ function simulateAsyncLoading(loadingBar, increment)
 
 function addResources()
 {
-    game.resources.dirt = game.resources.dirt.add(1);
-    document.getElementById("dirtAmount").innerHTML = game.resources.dirt;
+    //TODO: add code to figure out what types of resources to add based on what is unlocked
+    //For now, it just adds dirt
+    addDirt();
+}
+
+function addDirt()
+{
+    game.resources.dirt = game.resources.dirt.add(game.buyables.dirtUpgrades.dirtMiningPower);
 }
 
 function displayInfoText(textIndex)
@@ -183,14 +273,4 @@ function nextInfoText()
         game.infoTextLocation++;
         displayInfoText(game.infoTextLocation);
     }
-}
-
-function showTab(tabName)
-{
-    var tab = document.getElementsByClassName("tab");
-    for (var i = 0; i < tab.length; i++)
-    {
-        tab[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
 }
